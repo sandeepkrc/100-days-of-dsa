9537fd429d6142048ef19ae1744eebf6d1aa7fd9
Sandeep kumar <sandeepkumar@Sandeeps-MacBook-Pro.local>
2025-12-09 23:36:48 +0530

    Day 26: Added binary search solution for search insert position

---
+++ Day-26/ solution.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        start=0
+        end=len(nums)-1
+        while start<=end:
+            mid=(start+end)//2
+            if nums[mid]==target:
+                return mid
+            elif target>nums[mid]:
+                start= mid+1
+            else:
+                end=mid-1
+        return start
