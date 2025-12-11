23db1b1d70adf897e02a3e8236d1eb0437f78c17
Sandeep kumar <sandeepkumar@Sandeeps-MacBook-Pro.local>
2025-12-12 00:27:55 +0530

    Day 28: Implemented Dutch National Flag algorithm to sort colors (0s, 1s, 2s) in-place using three-pointer approach

---
+++ Day-28/ solution.py
@@ -0,0 +1,27 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+       """
+       function to sort colors in-place using Dutch National Flag algorithm
+       """
+        low=0
+        mid=0
+        high=len(nums)-1
+        
+        while mid<=high:
+            if nums[mid]==0:
+                self.swap(nums,low,mid)
+                mid=mid+1
+                low=low+1
+            elif nums[mid]==1:
+                mid=mid+1
+            else:
+                self.swap(nums,high,mid)
+                high=high-1
+
+    def swap(self,nums,i,j):
+            """
+            function to swap two elements in the list
+            """
+            k=nums[i]
+            nums[i]=nums[j]
+            nums[j]=k
