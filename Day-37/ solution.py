988d5bb161962bf4f0cee8539aaff0fd90fafb08
Sandeep kumar <sandeepkumar@Sandeeps-MacBook-Pro.local>
2025-12-22 00:48:27 +0530

    Day 37: Implement Min Stack with O(1) getMin operation

---
+++ Day-37/ solution.py
@@ -0,0 +1,37 @@
+
+class MinStack:
+
+    def __init__(self):
+        self.stack=[]
+        self.min_stack=[]
+         
+    def push(self, val: int) -> None:
+        if self.min_stack==[] or val<=self.min_stack[-1]:
+            self.min_stack.append(val)       
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        if self.stack:
+            val=self.stack.pop()
+            if val==self.min_stack[-1]:
+                self.min_stack.pop()
+
+
+    def top(self) -> int:
+        if self.stack:
+            return self.stack[-1]
+        return None
+    
+    def getMin(self) -> int:
+        if self.min_stack:
+            return self.min_stack[-1]
+        return None
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+obj = MinStack()
+obj.push(23)
+obj.pop()
+param_3 = obj.top()
+param_4 = obj.getMin()
