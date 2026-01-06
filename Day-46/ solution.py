25678fa678ed66175a6f7ba10d3a7f00c4fe30d4
Sandeep kumar <sandeepkumar@Sandeeps-MacBook-Pro.local>
2026-01-07 00:38:10 +0530

    Day 45: Implement removeDuplicateLetters using monotonic stack for lexicographically smallest result

---
+++ Day-46/ solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        rightsum=0
+        for ele in nums:
+            rightsum+=ele
+        leftsum=0
+        for i in range(len(nums)):
+            rightsum-=nums[i]
+            if rightsum==leftsum:
+                return i
+            leftsum+=nums[i]
+        return -1
